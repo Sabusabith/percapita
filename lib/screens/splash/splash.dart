52c52a34967be6d77620391b40210e9ad5aabe77
Sabith Mohammed <user@Sabith-MacBook-Pro.local>
2024-06-13 16:57:52 +0530

    commit after add loader

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:percapita_copy/common/colors/colors.dart';
 import 'package:percapita_copy/controller/splashcontroller.dart';
-
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 class SplashScreen extends StatelessWidget {
   
 
@@ -11,7 +12,10 @@ class SplashScreen extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Scaffold(backgroundColor: Colors.white,body: GetBuilder<SplashController>(init: SplashController(),builder: (controller) => 
       Container(width: size.width,height: size.height,
-        color: Colors.green,
+       child: Center(child: LoadingAnimationWidget.beat(
+        color: primeryColor.withOpacity(.7),
+        size: 100,
+      )),
         
       ),
     ),);
